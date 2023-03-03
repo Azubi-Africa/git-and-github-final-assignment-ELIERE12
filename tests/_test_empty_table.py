c620e43d227eda598f365c9a7bc9889f2ff5ecb4
ELIERE12 <126587187+ELIERE12@users.noreply.github.com>
2023-03-03 11:50:48 +0200

    fill table

--- tests/_test_empty_table.py
+++ tests/_test_empty_table.py
@@ -4,9 +4,9 @@ from tests.test_readme_table import read_table
 
 CURRENT_DIR = os.path.dirname(os.path.abspath(__file__))
 empty_json_table = {
-    "Project's Name": {1: "", 2: "", 3: ""},
-    "Description": {1: "", 2: "", 3: ""},
-    "GitHub's Link": {1: "", 2: "", 3: ""},
+    "Project's Name": {1: "eliere individual project", 2: "group8_Eliere week7 peer project", 3: "week5peerproject"},
+    "Description": {1: "project1", 2: "project2", 3: "project3"},
+    "GitHub's Link": {1: "https://github.com/ELIERE12/eliere-individual-project.git", 2: "https://github.com/ELIERE12/group8_Eliere-week7-peer-project.git", 3: "https://github.com/ELIERE12/week5peerproject.git"},
 }
 
 
